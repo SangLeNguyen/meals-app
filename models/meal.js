7b6921949378f390a83e2b3e8e19360672879e39
Sang <phusang2501@gmail.com>
2021-06-27 20:50:46 +0300

    update

--- models/meal.js
+++ models/meal.js
@@ -11,7 +11,21 @@ class Meal{
         isGlutenFree,
          isVegan, 
          isVegetarian, 
-         isLactoseFree){}
+         isLactoseFree){
+             this.id =id;
+             this.categoryIds= categoryIds;
+             this.title = title;
+             this.imageUrl = imageUrl;
+             this.ingredients = ingredients;
+             this.steps = steps;
+             this.duration =duration;
+             this.complexity = complexity;
+             this.affordability = affordability;
+             this.isglutenFree = isGlutenFree;
+             this.isVegan = isVegan;
+             this.isVegetarian = isVegetarian;
+             this.isLactoseFree = isLactoseFree
+         }
 }
 
 export default Meal;
